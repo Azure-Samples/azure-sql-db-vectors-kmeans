95e9bfedc4e6d717b6f4be021a82ba807830c9f7
Davide Mauri <damauri@microsoft.com>
2024-01-17 17:49:45 -0800

    added more columns

--- sql/query3.sql
+++ sql/query3.sql
@@ -1,1 +1,5 @@
-select id, [type], [class], item_count, vector_dimensions, data_version, saved_on from [$vector].[index]
+select 
+    id, source_table_name, id_column_name, vector_column_name, 
+    [type], [class], item_count, vector_dimensions, data_version, saved_on 
+from 
+    [$vector].[index]
