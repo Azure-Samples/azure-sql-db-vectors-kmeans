713c5e221f4fc0db578a750acdba01d1995b5b52
Davide Mauri <damauri@microsoft.com>
2024-01-23 20:12:36 +0000

    coherent path also in sql container

--- src/sql/00-import-data.sql
+++ src/sql/00-import-data.sql
@@ -6,7 +6,7 @@
 	Import data
 */
 bulk insert dbo.[wikipedia_articles_embeddings]
-from '/sample-data/vector_database_wikipedia_articles_embedded.csv'
+from '/src/sample-data/vector_database_wikipedia_articles_embedded.csv'
 with (
     format = 'csv',
     firstrow = 2,
